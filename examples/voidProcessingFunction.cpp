879f70d438ba0e9c8eed71aae255c390676024ea
Noam Ron <noam2009r@gmail.com>
2026-07-21 20:00:45 +0300

    Fix: fixed mistypes and #include

--- examples/voidProcessingFunction.cpp
+++ examples/voidProcessingFunction.cpp
@@ -1,20 +1,19 @@
-#include "../BetterSqlite3.h"
-#include <iostream>
+#include "BetterSqlite3.h"
 
 int main() {
     BetterSqlite3::BetterSqlite3 db("database.sqlite"); // opens / creates the database
 
     /*
-    Run the SQlite Command with no params:
+    Run the SQLite command with no params:
     CREATE TABLE Users (name TEXT);
     */
     db.bindAndExecuteCommand("CREATE TABLE IF NOT EXISTS Users (name TEXT);", {}); // ingores the return due to now return from SQLite.
 
     /*
-    Runs the Sqlite Command with name as param:
+    Runs the SQLite command with name as param:
     INSERT INTO Users (name) VALUES (?);
     */
-   std::string name = "noam";
-   db.bindAndExecuteCommand("INSERT INTO Users (name) VALUES (?);", {name});
+    std::string name = "noam";
+    db.bindAndExecuteCommand("INSERT INTO Users (name) VALUES (?);", {name});
 }
 
